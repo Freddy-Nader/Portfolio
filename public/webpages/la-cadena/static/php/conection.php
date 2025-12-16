6cf2a811c8c44c62acc245aaf300d59c0ec165ce
Alfredo Nader <anader@anahuac.mx>
2025-12-16 15:09:56 -0600

    Removed `public/webpages/ directory and added vercel links and a Websites section in `public/index.html`. It should be designed better, though, but works for now.

--- public/webpages/la-cadena/static/php/conection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = mysqli_connect("localhost", "root", "", "la_cadena");
-
-if (mysqli_connect_errno()) {
-    echo "<p>No se pudo realizar la conexión: " . mysqli_connect_errno() . "</p>";
-}
-?>
